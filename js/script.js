6028b098e30fe442ee2b3146705a319d12ddb1de
ryoAccount <jane.doe3wtp4649@gmail.com>
2016-07-18 17:52:53 +0900

    初めてGitHub使えた！

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* ===================================================================
+
+ * スマホ向けサブメニュー
+
+=================================================================== */
+$(function($){
+	var windowWidth = window.innerWidth || document.documentElement.clientWidth || 0;
+
+	function slideMenu() {
+		if ($('nav h3').css('display') == 'block') {
+			$('nav ul').hide();
+		}else {
+			$('nav ul').show();
+		}
+	}
+
+	$(window).on('load', function(){
+		slideMenu();
+	});
+
+	$(window).on('resize', function(){
+		var nowWidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if(windowWidth != nowWidth) {
+			slideMenu();
+		}
+		windowWidth = window.innerWidth || document.documentElement.clientWidth || 0;
+	});
+
+	$('nav h3').on('click', function(){
+		$(this).next().slideToggle();
+		$('#navBtnIcon').toggleClass('close');
+	});
+});
