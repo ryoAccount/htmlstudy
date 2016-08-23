54c035e7e59cf0e7e7c1cfbbc3d17a987f24e1d5
ryoAccount <jane.doe3wtp4649@gmail.com>
2016-08-23 14:47:45 +0900

    WALS 途中まで@2

--- WALS/js/original.js
+++ WALS/js/original.js
@@ -2,19 +2,19 @@ $('.sm1').click(function(){
 	$('.contents').fadeOut();
 	$('.sc1').fadeIn();
 	$('a').css('color','grey');
-	$('.sm1').css('color','black');
+	$('.sm1').css('color','rgb(200,0,0)');
 });
 
 $('.sm2').click(function(){
 	$('.contents').fadeOut();
 	$('.sc2').fadeIn(); 
 	$('a').css('color','grey');
-	$('.sm2').css('color','black');
+	$('.sm2').css('color','rgb(200,0,0)');
 });
 
 $('.sm3').click(function(){
 	$('.contents').fadeOut();
 	$('.sc3').fadeIn(); 
 	$('a').css('color','grey');
-	$('.sm3').css('color','black');
+	$('.sm3').css('color','rgb(200,0,0)');
 });
