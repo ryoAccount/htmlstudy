883657d4a3d2ae4997e260a3d4a708257be6564f
ryoAccount <jane.doe3wtp4649@gmail.com>
2016-08-23 23:34:26 +0900

    WALS 途中まで3

--- WALS/js/original.js
+++ WALS/js/original.js
@@ -18,3 +18,52 @@ $('.sm3').click(function(){
 	$('a').css('color','grey');
 	$('.sm3').css('color','rgb(200,0,0)');
 });
+
+$('.sm4').click(function(){
+	$('.contents').fadeOut();
+	$('.sc4').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm4').css('color','rgb(200,0,0)');
+});
+
+$('.sm5').click(function(){
+	$('.contents').fadeOut();
+	$('.sc5').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm5').css('color','rgb(200,0,0)');
+});
+
+$('.sm6').click(function(){
+	$('.contents').fadeOut();
+	$('.sc6').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm6').css('color','rgb(200,0,0)');
+});
+
+$('.sm7').click(function(){
+	$('.contents').fadeOut();
+	$('.sc7').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm7').css('color','rgb(200,0,0)');
+});
+
+$('.sm8').click(function(){
+	$('.contents').fadeOut();
+	$('.sc8').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm8').css('color','rgb(200,0,0)');
+});
+
+$('.sm9').click(function(){
+	$('.contents').fadeOut();
+	$('.sc9').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm9').css('color','rgb(200,0,0)');
+});
+
+$('.sm10').click(function(){
+	$('.contents').fadeOut();
+	$('.sc10').fadeIn(); 
+	$('a').css('color','grey');
+	$('.sm10').css('color','rgb(200,0,0)');
+});
