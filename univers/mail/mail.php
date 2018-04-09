6abda04460f04665c7750d4421d9dcbf371179df
ryoAccount <jane.doe3wtp4649@gmail.com>
2018-04-10 00:29:14 +0900

    My CV　メールフォーム修正

--- univers/mail/mail.php
+++ univers/mail/mail.php
@@ -1,136 +1,85 @@
 <?php header("Content-Type:text/html;charset=utf-8"); ?>
 <?php //error_reporting(E_ALL | E_STRICT);
-##-----------------------------------------------------------------------------------------------------------------##
-#
-#  PHPメールプログラム　フリー版 最終更新日2014/12/12
-#　改造や改変は自己責任で行ってください。
-#	
-#  今のところ特に問題点はありませんが、不具合等がありましたら下記までご連絡ください。
-#  MailAddress: info@php-factory.net
-#  name: K.Numata
-#  HP: http://www.php-factory.net/
-#
-#  重要！！サイトでチェックボックスを使用する場合のみですが。。。
-#  チェックボックスを使用する場合はinputタグに記述するname属性の値を必ず配列の形にしてください。
-#  例　name="当サイトをしったきっかけ[]"  として下さい。
-#  nameの値の最後に[と]を付ける。じゃないと複数の値を取得できません！
-#
-##-----------------------------------------------------------------------------------------------------------------##
-if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合のみタイムゾーンを定義
-	date_default_timezone_set('Asia/Tokyo');//タイムゾーンの設定（日本以外の場合には適宜設定ください）
+
+//PHP5.1.0以上の場合のみタイムゾーンを定義
+if (version_compare(PHP_VERSION, '5.1.0', '>=')) {
+	date_default_timezone_set('Asia/Tokyo');
 }
-/*-------------------------------------------------------------------------------------------------------------------
-* ★以下設定時の注意点　
-* ・値（=の後）は数字以外の文字列（一部を除く）はダブルクオーテーション「"」、または「'」で囲んでいます。
-* ・これをを外したり削除したりしないでください。後ろのセミコロン「;」も削除しないください。
-* ・また先頭に「$」が付いた文字列は変更しないでください。数字の1または0で設定しているものは必ず半角数字で設定下さい。
-* ・メールアドレスのname属性の値が「Email」ではない場合、以下必須設定箇所の「$Email」の値も変更下さい。
-* ・name属性の値に半角スペースは使用できません。
-*以上のことを間違えてしまうとプログラムが動作しなくなりますので注意下さい。
--------------------------------------------------------------------------------------------------------------------*/
 
-
-//---------------------------　必須設定　必ず設定してください　-----------------------
-
-//サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
+//---------------------------　必須設定　-----------------------
+//サイトのトップページのURL
 $site_top = "http://yuumin.chu.jp/mycv/index.html";
-
-// 管理者メールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
+// 管理者メールアドレス ※メールを受け取るメールアドレス(複数指定する場合「,」区切り $to = "aa@aa.aa,bb@bb.bb";)
 $to = "jane.doe3wtp4649@gmail.com";
-
-//フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
+//フォームのメールアドレス入力箇所のname属性の値
 $Email = "mailAdress";
-
-/*------------------------------------------------------------------------------------------------
-以下スパム防止のための設定　
-※有効にするにはこのファイルとフォームページが同一ドメイン内にある必要があります
-------------------------------------------------------------------------------------------------*/
-
 //スパム防止のためのリファラチェック（フォームページが同一ドメインであるかどうかのチェック）(する=1, しない=0)
-$Referer_check = 0;
-
-//リファラチェックを「する」場合のドメイン ※以下例を参考に設置するサイトのドメインを指定して下さい。
-$Referer_check_domain = "php-factory.net";
-
+$Referer_check = 1;
+//リファラチェックを「する」場合のドメイン
+$Referer_check_domain = "yuumin.chu.jp";
 //---------------------------　必須設定　ここまで　------------------------------------
 
-
 //---------------------- 任意設定　以下は必要に応じて設定してください ------------------------
-
-
 // 管理者宛のメールで差出人を送信者のメールアドレスにする(する=1, しない=0)
-// する場合は、メール入力欄のname属性の値を「$Email」で指定した値にしてください。
-//メーラーなどで返信する場合に便利なので「する」がおすすめです。
+// する場合、メール入力欄のname属性の値を「$Email」で指定した値にする
 $userMail = 1;
-
-// Bccで送るメールアドレス(複数指定する場合は「,」で区切ってください 例 $BccMail = "aa@aa.aa,bb@bb.bb";)
+// Bccで送るメールアドレス(複数指定する場合「,」で区切る 例 $BccMail = "aa@aa.aa,bb@bb.bb";)
 $BccMail = "";
-
 // 管理者宛に送信されるメールのタイトル（件名）
-$subject = "MyCVからのお問い合わせ";
-
+$subject = "Inquiries from My CV";
 // 送信確認画面の表示(する=1, しない=0)
 $confirmDsp = 1;
 
 // 送信完了後に自動的に指定のページ(サンクスページなど)に移動する(する=1, しない=0)
 // CV率を解析したい場合などはサンクスページを別途用意し、URLをこの下の項目で指定してください。
 // 0にすると、デフォルトの送信完了画面が表示されます。
-$jumpPage = 0;
-
-// 送信完了後に表示するページURL（上記で1を設定した場合のみ）※httpから始まるURLで指定ください。
-$thanksPage = "http://xxx.xxxxxxxxx/thanks.html";
-
+$jumpPage = 1;
+// 送信完了後に表示するページURL（上記で1を設定した場合のみ）
+$thanksPage = "http://yuumin.chu.jp/mycv/thanks.html";
 // 必須入力項目を設定する(する=1, しない=0)
 $requireCheck = 1;
-
-/* 必須入力項目(入力フォームで指定したname属性の値を指定してください。（上記で1を設定した場合のみ）
-値はシングルクォーテーションで囲み、複数の場合はカンマで区切ってください。フォーム側と順番を合わせると良いです。 
-配列の形「name="○○[]"」の場合には必ず後ろの[]を取ったものを指定して下さい。*/
-$require = array('mailAdress','subjectText','message');
-
+/* 必須入力項目(入力フォームで指定したname属性の値を指定（上記で1を設定した場合のみ）
+複数の場合カンマ区切り。フォーム側と順番を合わせると良いです。*/
+$require = array('Email','Subject','Message');
 
 //----------------------------------------------------------------------
 //  自動返信メール設定(START)
 //----------------------------------------------------------------------
 
 // 差出人に送信内容確認メール（自動返信メール）を送る(送る=1, 送らない=0)
-// 送る場合は、フォーム側のメール入力欄のname属性の値が上記「$Email」で指定した値と同じである必要があります
+// 送る場合は、フォーム側のメール入力欄のname属性の値が上記「$Email」で指定した値と同じである必要がある
 $remail = 0;
-
-//自動返信メールの送信者欄に表示される名前　※あなたの名前や会社名など（もし自動返信メールの送信者名が文字化けする場合ここは空にしてください）
+//自動返信メールの送信者欄に表示される名前
 $refrom_name = "";
-
 // 差出人に送信確認メールを送る場合のメールのタイトル（上記で1を設定した場合のみ）
-$re_subject = "送信ありがとうございました";
+$re_subject = "Thanks";
 
-//フォーム側の「名前」箇所のname属性の値　※自動返信メールの「○○様」の表示で使用します。
-//指定しない、または存在しない場合は、○○様と表示されないだけです。あえて無効にしてもOK
-$dsp_name = 'お名前';
+//自動返信メールの宛名（「○○様」の表示で使用）
+//未使用
+$dsp_name = '';
 
 //自動返信メールの冒頭の文言 ※日本語部分のみ変更可
 $remail_text = <<< TEXT
 
-お問い合わせありがとうございました。
-早急にご返信致しますので今しばらくお待ちください。
+Thank you for your inquiry.
+Please reply as soon as possible so please wait for a while.
 
-送信内容は以下になります。
+The transmission contents are as follows.
 
 TEXT;
 
 
-//自動返信メールに署名（フッター）を表示(する=1, しない=0)※管理者宛にも表示されます。
+//自動返信メールに署名（フッター）を表示(する=1, しない=0)
 $mailFooterDsp = 0;
-
-//上記で「1」を選択時に表示する署名（フッター）（FOOTER～FOOTER;の間に記述してください）
+//上記で「1」を選択時に表示する署名（フッター）
 $mailSignature = <<< FOOTER
 
 ──────────────────────
-株式会社○○○○　佐藤太郎
-〒150-XXXX 東京都○○区○○ 　○○ビル○F　
-TEL：03- XXXX - XXXX 　FAX：03- XXXX - XXXX
-携帯：090- XXXX - XXXX 　
-E-mail:xxxx@xxxx.com
-URL: http://www.php-factory.net/
+R.matsunami.
+1-11 SouthWood from Japan.　
+携帯：080- XXXX - XXXX 　
+E-mail:xxxx@gmail.com
+URL: http://yuumin.chu.jp/mycv/index.html/
 ──────────────────────
 
 FOOTER;
@@ -140,24 +89,13 @@ FOOTER;
 //  自動返信メール設定(END)
 //----------------------------------------------------------------------
 
-//メールアドレスの形式チェックを行うかどうか。(する=1, しない=0)
-//※デフォルトは「する」。特に理由がなければ変更しないで下さい。メール入力欄のname属性の値が上記「$Email」で指定した値である必要があります。
+//メールアドレスの形式チェック。(する=1, しない=0)
 $mail_check = 1;
-
 //全角英数字→半角変換を行うかどうか。(する=1, しない=0)
 $hankaku = 0;
-
-//全角英数字→半角変換を行う項目のname属性の値（name="○○"の「○○」部分）
-//※複数の場合にはカンマで区切って下さい。（上記で「1」を指定した場合のみ有効）
-//配列の形「name="○○[]"」の場合には必ず後ろの[]を取ったものを指定して下さい。
-$hankaku_array = array('電話番号','金額');
-
-
+//全角英数字→半角変換を行う項目のname属性の値（name="○○"の「○○」部分） 未使用
+$hankaku_array = array('','');
 //------------------------------- 任意設定ここまで ---------------------------------------------
-
-
-// 以下の変更は知識のある方のみ自己責任でお願いします。
-
 
 //----------------------------------------------------------------------
 //  関数実行、変数初期化
@@ -189,7 +127,7 @@ if(empty($errm)){
 		if($key == $Email) $post_mail = h($val);
 		if($key == $Email && $mail_check == 1 && !empty($val)){
 			if(!checkMail($val)){
-				$errm .= "<p class=\"error_messe\">【".$key."】はメールアドレスの形式が正しくありません。</p>\n";
+				$errm .= "<p class=\"error_messe\">【".$key."】 is an invalid email address format.</p>\n";
 				$empty_flag = 1;
 			}
 		}
@@ -214,15 +152,14 @@ if(($confirmDsp == 0 || $sendmail == 1) && $empty_flag != 1){
 }
 else if($confirmDsp == 1){ 
 
-/*　▼▼▼送信確認画面のレイアウト※編集可　オリジナルのデザインも適用可能▼▼▼　*/
+/*　▼▼▼送信確認画面のレイアウト▼▼▼　*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>確認画面</title>
+<title>Confirmation</title>
 <style type="text/css">
-/* 自由に編集下さい */
 #formWrap {
 	width:700px;
 	margin:0 auto;
@@ -253,79 +190,76 @@ p.error_messe{
 </head>
 <body>
 
-<!-- ▲ Headerやその他コンテンツなど　※自由に編集可 ▲-->
+<!-- ▲ Headerやその他コンテンツなど ▲-->
 
-<!-- ▼************ 送信内容表示部　※編集は自己責任で ************ ▼-->
+<!-- ▼************ 送信内容表示部 ************ ▼-->
 <div id="formWrap">
 <?php if($empty_flag == 1){ ?>
 <div align="center">
-<h4>入力にエラーがあります。下記をご確認の上「戻る」ボタンにて修正をお願い致します。</h4>
-<?php echo $errm; ?><br /><br /><input type="button" value=" 前画面に戻る " onClick="history.back()">
+<h4>There is an error in the input. Please check the following and correct by the "return" button.</h4>
+<?php echo $errm; ?><br /><br /><input type="button" value=" return " onClick="history.back()">
 </div>
 <?php }else{ ?>
-<h3>確認画面</h3>
-<p align="center">以下の内容で間違いがなければ、「送信する」ボタンを押してください。</p>
+<h3>Confirmation</h3>
+<p align="center">Please press the "send" button.</p>
 <form action="<?php echo h($_SERVER['SCRIPT_NAME']); ?>" method="POST">
 <table class="formTable">
 <?php echo confirmOutput($_POST);//入力内容を表示?>
 </table>
 <p align="center"><input type="hidden" name="mail_set" value="confirm_submit">
 <input type="hidden" name="httpReferer" value="<?php echo h($_SERVER['HTTP_REFERER']);?>">
-<input type="submit" value="　送信する　">
-<input type="button" value="前画面に戻る" onClick="history.back()"></p>
+<input type="submit" value=" send ">
+<input type="button" value=" return " onClick="history.back()"></p>
 </form>
 <?php } ?>
 </div><!-- /formWrap -->
-<!-- ▲ *********** 送信内容確認部　※編集は自己責任で ************ ▲-->
+<!-- ▲ *********** 送信内容確認部 ************ ▲-->
 
-<!-- ▼ Footerその他コンテンツなど　※編集可 ▼-->
+<!-- ▼ Footerその他コンテンツなど ▼-->
 </body>
 </html>
 <?php
-/* ▲▲▲送信確認画面のレイアウト　※オリジナルのデザインも適用可能▲▲▲　*/
+/* ▲▲▲送信確認画面のレイアウト▲▲▲　*/
 }
 
 if(($jumpPage == 0 && $sendmail == 1) || ($jumpPage == 0 && ($confirmDsp == 0 && $sendmail == 0))) { 
 
-/* ▼▼▼送信完了画面のレイアウト　編集可 ※送信完了後に指定のページに移動しない場合のみ表示▼▼▼　*/
+/* ▼▼▼送信完了画面のレイアウト ※送信完了後に指定のページに移動しない場合のみ表示▼▼▼　*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>完了画面</title>
+<title>Done</title>
 </head>
 <body>
 <div align="center">
 <?php if($empty_flag == 1){ ?>
-<h4>入力にエラーがあります。下記をご確認の上「戻る」ボタンにて修正をお願い致します。</h4>
+<h4>There is an error in the input. Please check the following and correct by the "return" button.</h4>
 <div style="color:red"><?php echo $errm; ?></div>
-<br /><br /><input type="button" value=" 前画面に戻る " onClick="history.back()">
+<br /><br /><input type="button" value=" return " onClick="history.back()">
 </div>
 </body>
 </html>
 <?php }else{ ?>
-送信ありがとうございました。<br />
-送信は正常に完了しました。<br /><br />
-<a href="<?php echo $site_top ;?>">トップページへ戻る&raquo;</a>
+Thanks.<br /><br />
+<a href="<?php echo $site_top ;?>">Top&raquo;</a>
 </div>
 <?php copyright(); ?>
 <!--  CV率を計測する場合ここにAnalyticsコードを貼り付け -->
 </body>
 </html>
 <?php 
-/* ▲▲▲送信完了画面のレイアウト 編集可 ※送信完了後に指定のページに移動しない場合のみ表示▲▲▲　*/
+/* ▲▲▲送信完了画面のレイアウト ※送信完了後に指定のページに移動しない場合のみ表示▲▲▲　*/
   }
 }
 //確認画面無しの場合の表示、指定のページに移動する設定の場合、エラーチェックで問題が無ければ指定ページヘリダイレクト
 else if(($jumpPage == 1 && $sendmail == 1) || $confirmDsp == 0) { 
 	if($empty_flag == 1){ ?>
-<div align="center"><h4>入力にエラーがあります。下記をご確認の上「戻る」ボタンにて修正をお願い致します。</h4><div style="color:red"><?php echo $errm; ?></div><br /><br /><input type="button" value=" 前画面に戻る " onClick="history.back()"></div>
+<div align="center"><h4>There is an error in the input. Please check the following and correct by the "return" button.</h4><div style="color:red"><?php echo $errm; ?></div><br /><br /><input type="button" value=" return " onClick="history.back()"></div>
 <?php 
 	}else{ header("Location: ".$thanksPage); }
 }
-
-// 以下の変更は知識のある方のみ自己責任でお願いします。
 
 //----------------------------------------------------------------------
 //  関数定義(START)
