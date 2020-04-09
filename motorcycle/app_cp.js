f57b20e89fd78f0afdf12001e43eb8283467a269
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2020-04-09 17:07:21 +0900

    バイクゲーム仮作成

---
+++ motorcycle/app_cp.js
@@ -0,0 +1,106 @@
+var c = document.createElement("canvas");
+var ctx = c.getContext("2d");
+c.width = window.innerWidth;
+c.height = window.innerHeight - 5;
+document.body.appendChild(c);
+
+var perm = [];
+while (perm.length < 255) {
+    while (perm.includes(val = Math.floor(Math.random()*255)));
+    perm.push(val);
+}
+
+var lerp = (a,b,t) => a + (b-a) * (1 - Math.cos(t * Math.PI))/2;
+var noise = x => {
+    x = x * 0.01 % 255;
+    return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
+
+var player = new function() {
+    this.x = c.width/2;
+    this.y = 0;
+    this.ySpeed = 0;
+    this.rot = 0;
+    this.rSpeed = 0;
+
+    this.img = new Image();
+    this.img.src = 'moto.png';
+    this.draw = function() {
+        var p1 = c.height - noise(t + this.x) * 0.25;
+        var p2 = c.height - noise(t + 5 + this.x) * 0.25;
+
+        var grounded = 0;
+        if ( p1 -15 > this.y) {
+            this.ySpeed += 0.1;
+        } else {
+            this.ySpeed -= this.y - (p1 - 12);
+            this.y = p1 - 12;
+            grounded = 1;
+        }
+
+        if(grounded && Math.abs(this.rot) > Math.PI * 0.5) {
+            this.rSpeed = 5;
+            this.x -= speed * 2.5;
+        }
+
+        var angle = Math.atan2((p2 - 11) - this.y, (this.x + 5) - this.x);
+        this.y += this.ySpeed;
+
+        if(grounded) {
+            this.rot -= (this.rot - angle) * 0.5;
+            this.rSpeed = this.rSpeed - (angle - this.rot);
+        }
+
+        this.rSpeed += 1 * 0.05;
+        this.rot -= this.rSpeed * 0.1;
+        if(this.rot > Math.PI) {
+            this.rot = -Math.PI;
+        }
+        if(this.rot < -Math.PI) {
+            this.rot = Math.PI;
+        }
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rot);
+        ctx.drawImage(this.img, -15, -15, 30, 30);
+        ctx.restore();
+    }
+}
+
+var t = 0;
+function loop() {
+    t += 5;
+    ctx.fillStyle = "#17f";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(0, c.height);
+    for (let i = 0 ; i < c.width ; i++) {
+        ctx.lineTo(i, c.height - noise(t + i) * 0.25);
+    }
+
+    ctx.lineTo(c.width, c.height);
+    ctx.fill();
+
+    player.draw();
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+const startDate = new Date();
+function playTime() {
+    let now = new Date();
+    let sec = parseInt((now.getTime() - startDate.getTime()) / 1000) % 60;
+
+    if ( sec < 10) {
+        sec = "0" + sec;
+    }
+
+    document.getElementById("times").innerHTML = sec + " seconds."
+
+    this.setTimeout(playTime(), 1000);
+}
+playTime();
