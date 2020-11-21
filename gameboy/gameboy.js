79f98bf974ebebb721a35cc4c772a0f125b35599
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2020-11-21 17:30:51 +0900

    gameboy js

---
+++ gameboy/gameboy.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    const SWITCH = document.querySelector('.switch');
+    const SE = document.querySelector('.se');
+    const LAMP = document.querySelector('.lamp');
+    const LOGO = document.querySelector('.logo');
+
+    let isStart = false;
+    let timer = 0;
+
+    SWITCH.addEventListener('click', function() {
+        if(isStart) {
+            // ON -> OFF
+            SWITCH.classList.remove('on');
+            LOGO.classList.remove('on');
+            LAMP.classList.remove('lamp-on');
+
+            window.clearTimeout(timer);
+        } else {
+            // OFF -> ON
+            SWITCH.classList.add('on');
+            LOGO.classList.add('on');
+            LAMP.classList.add('lamp-on');
+
+            timer = window.setTimeout(() => {
+                SE.currentTime = 0;
+                SE.play();
+            }, 3000);
+        }
+        isStart = !isStart;
+    });
+}
+
