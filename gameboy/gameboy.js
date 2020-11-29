97b4b7b7ae5c5ed3c452bb73d413c6486a7681ac
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2020-11-30 06:15:08 +0900

    gameboy css

--- gameboy/gameboy.js
+++ gameboy/gameboy.js
@@ -11,14 +11,14 @@ window.onload = () => {
         if(isStart) {
             // ON -> OFF
             SWITCH.classList.remove('on');
-            LOGO.classList.remove('on');
+            LOGO.classList.remove('disp');
             LAMP.classList.remove('lamp-on');
 
             window.clearTimeout(timer);
         } else {
             // OFF -> ON
             SWITCH.classList.add('on');
-            LOGO.classList.add('on');
+            LOGO.classList.add('disp');
             LAMP.classList.add('lamp-on');
 
             timer = window.setTimeout(() => {
