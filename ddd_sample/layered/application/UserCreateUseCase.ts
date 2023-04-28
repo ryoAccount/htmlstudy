2fa5ceb4b8fe118eb86bde44303abbaa54edece5
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-04-29 03:13:33 +0900

    layered

---
+++ ddd_sample/layered/application/UserCreateUseCase.ts
@@ -0,0 +1,15 @@
+import { User } from "../domain/User";
+import { UserRepository } from "../infrastructure/UserRepository";
+
+export class UserCreateUseCase {
+  constructor() {}
+
+  async handler(name: string, email: string): Promise<void> {
+    try {
+      const user = new User(name, email);
+      await new UserRepository().save(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
